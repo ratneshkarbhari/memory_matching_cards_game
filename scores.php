59f436a4a0cfbb4b378d74a81535f3069b50e529
Ratnesh Karbhari <ratnesh@pop-os.localdomain>
2020-09-29 13:18:12 +0530

    adding game to github

---
+++ scores.php
@@ -0,0 +1,33 @@
+
+<!Doctype html>
+<html lang="fr">
+        <head>
+                <meta charset="UTF-8">
+                <title>Scores | JeuUP8</title>
+                <link rel="stylesheet" type="text/css" href="./style.css">
+                <script src="./script.js"></script>
+        </head>
+        <script>
+                console.log("Hello world!");
+        </script>
+        <body>
+                <header>
+                        <nav>
+                                <ul>
+                                        <li><a href="./index.html">Jeu</a></li>
+                                        <li><a href="./scores.html">Scores</a></li>
+                                </ul>
+                        </nav>
+                </header>
+                <main>
+					<div class="leaderboard">
+						<div class="label">
+							<img src="https://user-images.githubusercontent.com/23297041/55285200-a24e9b00-538f-11e9-8990-d49a162824d1.png" height="42" width="42">
+							<h1>Clasification</h1>
+						</div>
+						<ol class="rank">
+							<li><strong>Ratnesh</strong><span>56</span></li>						</ol>
+					</div>
+                </main>
+        </body>
+</html>
